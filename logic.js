08a2e0b7d4b1609678a76eb6ed41cbaa4aa6f470
Jaycee Dela Rosa <88520055+Ciarra1@users.noreply.github.com.>
2025-08-26 17:21:31 +0800

    completed the ui functionality of the website... javascript logic to be added.

---
+++ logic.js
@@ -0,0 +1,5 @@
+let username = document.querySelector(".username");
+let username_p = document.querySelector(".username_p");
+username.textContent = "Jaycee Dela Rosa (@ciarra)" ;
+username_p.textContent = "Jaycee Dela Rosa";
+
